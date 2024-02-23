426463b8f73c6c8f2e55c116a4db99348fb6d7c6
thorium <102315529+thorium-cfx@users.noreply.github.com>
2024-02-23 14:37:05 +0100

    Rename testing platform type members to Uppercase
    * Fixes compilation

--- MsgPack/PlatformTypes/Vectors.cs
+++ MsgPack/PlatformTypes/Vectors.cs
@@ -4,81 +4,81 @@ namespace CitizenFX.Core
 {
     public struct Vector2
     {
-        public float x, y;
+        public float X, Y;
 
         public Vector2(float x, float y)
         {
-            this.x = x;
-            this.y = y;
+            this.X = x;
+            this.Y = y;
 		}
 
-		public override string ToString() => $"{nameof(Vector2)}({x}, {y})";
-		public static bool operator ==(Vector2 l, Vector2 r) => l.x == r.x && l.y == r.y;
+		public override string ToString() => $"{nameof(Vector2)}({X}, {Y})";
+		public static bool operator ==(Vector2 l, Vector2 r) => l.X == r.X && l.Y == r.Y;
 		public static bool operator !=(Vector2 l, Vector2 r) => !(l == r);
 		public override bool Equals(object o) => o is Vector2 v && this == v;
-		public override int GetHashCode() => x.GetHashCode() ^ y.GetHashCode();
+		public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
 	}
 
     [MsgPackSerializable(Layout.Indexed)]
     public struct Vector3
     {
-        [Index(0)] public float x;
-		[Index(1)] public float y;
-		[Index(2)] public float z;
+        [Index(0)] public float X;
+		[Index(1)] public float Y;
+		[Index(2)] public float Z;
 
         public Vector3(float x, float y, float z)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
         }
 
-        public override string ToString() => $"{nameof(Vector3)}({x}, {y}, {z})";
-        public static bool operator ==(Vector3 l, Vector3 r) => l.x == r.x && l.y == r.y && l.z == r.z;
+        public override string ToString() => $"{nameof(Vector3)}({X}, {Y}, {Z})";
+        public static bool operator ==(Vector3 l, Vector3 r) => l.X == r.X && l.Y == r.Y && l.Z == r.Z;
         public static bool operator !=(Vector3 l, Vector3 r) => !(l == r);
         public override bool Equals(object o) => o is Vector3 v && this == v;
-		public override int GetHashCode() => x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode();
+		public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode();
 	}
 
     public struct Vector4
     {
-        public float x, y, z, w;
+        public float X, Y, Z, W;
 
         public Vector4(float x, float y, float z, float w)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.w = w;
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.W = w;
 		}
 
-		public override string ToString() => $"{nameof(Vector4)}({x}, {y}, {z}, {w})";
-		public static bool operator ==(Vector4 l, Vector4 r) => l.x == r.x && l.y == r.y && l.z == r.z && l.w == r.w;
+		public override string ToString() => $"{nameof(Vector4)}({X}, {Y}, {Z}, {W})";
+		public static bool operator ==(Vector4 l, Vector4 r) => l.X == r.X && l.Y == r.Y && l.Z == r.Z && l.W == r.W;
 		public static bool operator !=(Vector4 l, Vector4 r) => !(l == r);
 		public override bool Equals(object o) => o is Vector4 v && this == v;
-		public override int GetHashCode() => x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode() ^ w.GetHashCode();
+		public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode() ^ W.GetHashCode();
 	}
 
 	[MsgPackSerializable(Layout.Indexed)]
 	public struct Quaternion
 	{
-		[Index(0)] public float x;
-		[Index(1)] public float y;
-		[Index(2)] public float z;
-		[Index(3)] public float w;
+		[Index(0)] public float X;
+		[Index(1)] public float Y;
+		[Index(2)] public float Z;
+		[Index(3)] public float W;
 
 		public Quaternion(float x, float y, float z, float w)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.w = w;
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.W = w;
 		}
 
-		public override string ToString() => $"{nameof(Quaternion)}({x}, {y}, {z}, {w})";
-		public static bool operator ==(Quaternion l, Quaternion r) => l.x == r.x && l.y == r.y && l.z == r.z && l.w == r.w;
+		public override string ToString() => $"{nameof(Quaternion)}({X}, {Y}, {Z}, {W})";
+		public static bool operator ==(Quaternion l, Quaternion r) => l.X == r.X && l.Y == r.Y && l.Z == r.Z && l.W == r.W;
 		public static bool operator !=(Quaternion l, Quaternion r) => !(l == r);
 		public override bool Equals(object o) => o is Quaternion v && this == v;
-		public override int GetHashCode() => x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode() ^ w.GetHashCode();
+		public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode() ^ W.GetHashCode();
 	}
 }
