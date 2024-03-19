e73fa07fc66766223f8f0185a66c2c9c88db9a26
thorium <102315529+thorium-cfx@users.noreply.github.com>
2024-03-19 16:16:41 +0100

    Simplify and remove duplicated code in unit-tests
    * Replace `SerializeTest<T>` struct with a single `Validate<T>(...)` method

--- MsgPack.Tests/Serialize.cs
+++ MsgPack.Tests/Serialize.cs
@@ -8,23 +8,13 @@ namespace CitizenFX.MsgPack.Tests
 	[TestClass]
 	public class Serialize
 	{
-		private struct SerializeTest<T>
+		private static void Validate<T>(T value, byte[] expectedResult)
 		{
-			public T Input { private set; get; }
-			public byte[] ExpectedResult { private set; get; }
-
-			public SerializeTest(T input, byte[] expectedResult)
-			{
-				Input = input;
-				ExpectedResult = expectedResult;
-			}
-
-			public void AssertValidate(MsgPackSerializer serializer)
-			{
-				byte[] result = serializer.ToArray();
-                Assert.IsTrue(ExpectedResult.SequenceEqual(result), $"{Input} is incorrectly serialized, got: `{BitConverter.ToString(result)}`, expected: `{BitConverter.ToString(ExpectedResult)}`");
-			}
-        }
+			MsgPackSerializer serializer = new MsgPackSerializer();
+			serializer.Serialize(value);
+			byte[] result = serializer.ToArray();
+			Assert.IsTrue(expectedResult.SequenceEqual(result), $"{value} is incorrectly serialized,\n       got: `{BitConverter.ToString(result)}`\n  expected: `{BitConverter.ToString(expectedResult)}`");
+		}
 
 		[TestInitialize]
 		public void _Dummy()
@@ -39,143 +29,75 @@ namespace CitizenFX.MsgPack.Tests
 		[TestMethod]
 		public void SerializeIntegers()
 		{
-            SerializeTest<int>[] tests =
-            {
-				new SerializeTest<int>(1, new byte[] { 0x01 }),
-				new SerializeTest<int>(-1, new byte[] { 0xFF }),
-				new SerializeTest<int>(-2, new byte[] { 0xFE }),
-				new SerializeTest<int>(-128, new byte[] { 0xD0, 0x80 }),
-				new SerializeTest<int>(-129, new byte[] { 0xD1, 0xFF, 0x7F }),
-				
-			};
-
-			for (int i = 0; i < tests.Length; ++i)
-			{
-				var test = tests[i];
-
-				MsgPackSerializer serializer = new MsgPackSerializer();
-				serializer.Serialize(test.Input);
-				test.AssertValidate(serializer);
-			}
+			Validate(1, new byte[] { 0x01 });
+			Validate(-1, new byte[] { 0xFF });
+			Validate(-2, new byte[] { 0xFE });
+			Validate(-128, new byte[] { 0xD0, 0x80 });
+			Validate(-129, new byte[] { 0xD1, 0xFF, 0x7F });
 		}
 
 		[TestMethod]
 		public void SerializeFloat()
 		{
-			{
-				MsgPackSerializer serializer = new MsgPackSerializer();
-				var test = new SerializeTest<float>(9999.2312312f, new byte[] { 0xCA, 0x46, 0x1C, 0x3C, 0xED });
-				serializer.Serialize(test.Input);
-                test.AssertValidate(serializer);
-            }
-
-			{
-				MsgPackSerializer serializer = new MsgPackSerializer();
-                var test = new SerializeTest<double>(123456.455463, new byte[] { 0xCB, 0x40, 0xFE, 0x24, 0x07, 0x49, 0x93, 0x92, 0x19 });
-                serializer.Serialize(test.Input);
-                test.AssertValidate(serializer);
-            }
+			Validate(9999.2312312f, new byte[] { 0xCA, 0x46, 0x1C, 0x3C, 0xED });
+			Validate(123456.455463, new byte[] { 0xCB, 0x40, 0xFE, 0x24, 0x07, 0x49, 0x93, 0x92, 0x19 });
 		}
 
 		[TestMethod]
 		public void SerializeDictionaryStringStringAsObject()
 		{
-			MsgPackSerializer serializer = new MsgPackSerializer();
-
-			serializer.Serialize(new Dictionary<string, string>
-			{
-				{ "hello0", "item0" },
-				{ "hello1", "item1" },
-				{ "hello2", "item2" },
-				{ "hello3", "item3" },
-			});
-
-			byte[] result = serializer.ToArray();
-			byte[] expect = new byte[]
-			{
-				0x84,                                     // map of 4 items
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x30, // key
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x30,       // value
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x31,
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x31,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32,
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x32,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x33,
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x33
-			};
-
-			Assert.IsTrue(result.SequenceEqual(expect), $"Incorrectly serialized, got: `{BitConverter.ToString(result)}`, expected: `{BitConverter.ToString(expect)}`");
+			Validate(
+				new Dictionary<string, string>
+				{
+					{ "hello0", "item0" },
+					{ "hello1", "item1" },
+					{ "hello2", "item2" },
+					{ "hello3", "item3" },
+				}, new byte[]
+				{
+					0x84,                                     // map of 4 items
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x30, // key
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x30,       // value
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x31,
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x31,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32,
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x32,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x33,
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x33
+				}
+			);
 		}
 
 		[TestMethod]
 		public void SerializeDictionaryStringIntAsObject()
 		{
-			MsgPackSerializer serializer = new MsgPackSerializer();
-
-			serializer.Serialize(new Dictionary<int, string>
-			{
-				{ -2, "item0" },
-				{ 200_000, "item1" },
-				{ 1234, "item2" },
-				{ -9000, "item3" },
-			});
-
-			byte[] result = serializer.ToArray();
-			byte[] expect = new byte[]
-			{
-				0x84,                                     // map of 4 items
-				0xFE,                                     // negative fixint -2
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x30,       // value
-				0xCE, 0x00, 0x03, 0x0D, 0x40,             // uint32 200,000
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x31,
-				0xCD, 0x04, 0xD2,                         // uint16 1234
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x32,
-				0xD1, 0xDC, 0xD8,                         // int16 -9000
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x33
-			};
-
-			Assert.IsTrue(result.SequenceEqual(expect), $"Incorrectly serialized, got: `{BitConverter.ToString(result)}`, expected: `{BitConverter.ToString(expect)}`");
+			Validate(
+				new Dictionary<int, string>
+				{
+					{ -2, "item0" },
+					{ 200_000, "item1" },
+					{ 1234, "item2" },
+					{ -9000, "item3" },
+				}, new byte[]
+				{
+					0x84,                                     // map of 4 items
+					0xFE,                                     // negative fixint -2
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x30,       // value
+					0xCE, 0x00, 0x03, 0x0D, 0x40,             // uint32 200,000
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x31,
+					0xCD, 0x04, 0xD2,                         // uint16 1234
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x32,
+					0xD1, 0xDC, 0xD8,                         // int16 -9000
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x33
+				}
+			);
 		}
 
 
 		[TestMethod]
 		public void SerializeArrayStringAsObject()
 		{
-			MsgPackSerializer serializer = new MsgPackSerializer();
-
-			serializer.Serialize(new string[]
-			{
-				"hello0",
-				"hello1",
-				"hello2",
-				"hello3",
-				"hello4",
-				"hello5",
-			});
-
-			byte[] result = serializer.ToArray();
-			byte[] expect = new byte[]
-			{
-				0x96,                                     // array of 6 items
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x30, // values
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x31,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x33,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x34,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x35
-			};
-
-			Assert.IsTrue(result.SequenceEqual(expect), $"Incorrectly serialized, got: `{BitConverter.ToString(result)}`, expected: `{BitConverter.ToString(expect)}`");
-		}
-
-
-		[TestMethod]
-		public void SerializeObjectArray()
-		{
-			MsgPackSerializer serializer = new MsgPackSerializer();
-
-			object[] arguments = new object[]
-			{
+			Validate(
 				new string[]
 				{
 					"hello0",
@@ -184,63 +106,85 @@ namespace CitizenFX.MsgPack.Tests
 					"hello3",
 					"hello4",
 					"hello5",
-				},
-				new Dictionary<int, string>
+				}, new byte[]
 				{
-					{ -2, "item0" },
-					{ 200_000, "item1" },
-					{ 1234, "item2" },
-					{ -9000, "item3" },
-				},
-				true,
-				false,
-				-1,
-				1,
-				99999,
-				123456778,
-				null,
-				12345.0,
-				87391973912.23342f
-			};
+					0x96,                                     // array of 6 items
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x30, // values
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x31,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x33,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x34,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x35
+				}
+			);
+		}
 
-			serializer.Serialize(arguments);
 
-			byte[] result = serializer.ToArray();
-			byte[] expect = new byte[]
-			{
-				0x9B,                                     // array of 11 items
+		[TestMethod]
+		public void SerializeObjectArray()
+		{
+			Validate(
+				new object[]
+				{
+					new string[]
+					{
+						"hello0",
+						"hello1",
+						"hello2",
+						"hello3",
+						"hello4",
+						"hello5",
+					},
+					new Dictionary<int, string>
+					{
+						{ -2, "item0" },
+						{ 200_000, "item1" },
+						{ 1234, "item2" },
+						{ -9000, "item3" },
+					},
+					true,
+					false,
+					-1,
+					1,
+					99999,
+					123456778,
+					null,
+					12345.0,
+					87391973912.23342f
+				}, new byte[]
+				{
+					0x9B,                                     // array of 11 items
 				
-				0x96,                                     // array of 6 items
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x30, // string[] values
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x31,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x33,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x34,
-				0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x35,
+					0x96,                                     // array of 6 items
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x30, // string[] values
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x31,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x33,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x34,
+					0xA6, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x35,
 
-				0x84,                                     // map of 4 items
-				0xFE,                                     // negative fixint -2
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x30,       // value
-				0xCE, 0x00, 0x03, 0x0D, 0x40,             // uint32 200,000
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x31,
-				0xCD, 0x04, 0xD2,                         // uint16 1234
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x32,
-				0xD1, 0xDC, 0xD8,                         // int16 -9000
-				0xA5, 0x69, 0x74, 0x65, 0x6D, 0x33,
+					0x84,                                     // map of 4 items
+					0xFE,                                     // negative fixint -2
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x30,       // value
+					0xCE, 0x00, 0x03, 0x0D, 0x40,             // uint32 200,000
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x31,
+					0xCD, 0x04, 0xD2,                         // uint16 1234
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x32,
+					0xD1, 0xDC, 0xD8,                         // int16 -9000
+					0xA5, 0x69, 0x74, 0x65, 0x6D, 0x33,
 
-				0xC3,                                     // true
-				0xC2,                                     // false
-				0xFF,                                     // -1
-				0x01,                                     // 1
-				0xCE, 0x00, 0x01, 0x86, 0x9F,             // 99999
-				0xCE, 0x07, 0x5B, 0xCD, 0x0A,             // 123456778
-				0xC0,                                     // null
+					0xC3,                                     // true
+					0xC2,                                     // false
+					0xFF,                                     // -1
+					0x01,                                     // 1
+					0xCE, 0x00, 0x01, 0x86, 0x9F,             // 99999
+					0xCE, 0x07, 0x5B, 0xCD, 0x0A,             // 123456778
+					0xC0,                                     // null
 
-				0xCB, 0x40, 0xC8, 0x1C, 0x80, 0x00, 0x00, 0x00, 0x00, // 12345.0
-				0xCA, 0x51, 0xA2, 0xC7, 0xBE              // 87391973912.23342f
-			};
-
-			Assert.IsTrue(result.SequenceEqual(expect), $"Incorrectly serialized, got: `{BitConverter.ToString(result)}`, expected: `{BitConverter.ToString(expect)}`");
+					0xCB, 0x40, 0xC8, 0x1C, 0x80, 0x00, 0x00, 0x00, 0x00, // 12345.0
+					0xCA, 0x51, 0xA2, 0xC7, 0xBE              // 87391973912.23342f
+				}
+			);
 		}
 	}
 }
