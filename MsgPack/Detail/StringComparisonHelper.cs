59c18658cd8b7457408c19d0a3c29ff93d58dd5c
Yauhen Pahrabniak <yauhen.pahrabniak@rockstargames.com>
2024-05-28 16:26:00 +0200

    Fixed compilation on linux

--- MsgPack/Detail/StringComparisonHelper.cs
+++ MsgPack/Detail/StringComparisonHelper.cs
@@ -3,22 +3,25 @@ using System.Runtime.InteropServices;
 
 namespace CitizenFX.MsgPack.Detail
 {
-	[StructLayout(LayoutKind.Explicit)]
-	internal unsafe struct StringComparisonHelper
-	{
-		[FieldOffset(0)] public fixed byte u8[8];
-		[FieldOffset(0)] public fixed ushort u16[4];
-		[FieldOffset(0)] public fixed uint u32[2];
-		[FieldOffset(0)] public fixed ulong u64[1];
+    [StructLayout(LayoutKind.Explicit)]
+    internal unsafe struct StringComparisonHelper
+    {
+        [FieldOffset(0)] public fixed byte u8[8];
+        [FieldOffset(0)] public fixed ushort u16[4];
+        [FieldOffset(0)] public fixed uint u32[2];
+        [FieldOffset(0)] public fixed ulong u64[1];
 
-		public StringComparisonHelper(string str, int offset = 0)
-		{
-			int length = str.Length - offset;
-			if (length > 8)
-				length = 8;
+        public StringComparisonHelper(string str, int offset = 0)
+        {
+            fixed (byte* pU8 = u8)
+            {
+                int length = str.Length - offset;
+                if (length > 8)
+                    length = 8;
 
-			for (int i = 0; i < length; ++i)
-				u8[i] = (byte)str[i - offset];
-		}
-	}
+                for (int i = 0; i < length; ++i)
+                    pU8[i] = (byte)str[i + offset];
+            }
+        }
+    }
 }
